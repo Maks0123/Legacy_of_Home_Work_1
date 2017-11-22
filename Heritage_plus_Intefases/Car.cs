6e40ba140ae1b313331927942c2e9c9416abe3e2
Vadim Taylor <VadimCorporateUkr@gmail.com>
2017-11-22 23:53:53 +0200

    Secon projects

---
+++ Heritage_plus_Intefases/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage_plus_Intefases
+{
+
+    interface ICar
+    {
+        int HorsePower { get; set; }
+        int Speed { get; set; }
+        string Color { get; set; }
+    }
+
+    class Car : ICar
+    {
+    Car() { }
+
+        public int HorsePower {
+            get; set;
+        }
+        public int Speed {
+            get; set;
+        }
+        public string Color {
+            get; set;
+        }
+    }
+}
+
+
+
---
+++ Heritage_plus_Intefases/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage_plus_Intefases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
